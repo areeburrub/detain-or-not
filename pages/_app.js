bc08b43c19f3bbcf882f4df4f52b06b23a855cb4
areeburrub <areeburrub@gmail.com>
2022-05-13 23:19:27 +0530

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
---
+++ pages/api/getAtt.js
@@ -0,0 +1,128 @@
+import puppeteer from "puppeteer";
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    if (obj[key] !== null && obj[key] !== "") return false;
+  }
+  return true;
+}
+
+const handler = async (req, res) => {
+  const ad_number = req.query.adno;
+  const pswd = req.query.pswd || "GCET123";
+  if (!ad_number) {
+    res.status(400).send({ error: "Admission number is not found!" });
+  } else {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto("https://gu.icloudems.com");
+
+    await page.waitForSelector("#useriid");
+    await page.focus("#useriid");
+    await page.keyboard.type(ad_number);
+
+    await page.waitForSelector("#actlpass");
+    await page.focus("#actlpass");
+    await page.keyboard.type(pswd);
+
+    await page.waitForTimeout(1500);
+
+    await (await page.$("#psslogin")).press("Enter"); // Enter Key
+
+    await page.waitForTimeout(1500);
+
+    if (
+      page
+        .url()
+        .includes("errormessage=Invalid+Username+or+Password.Please+try+again.")
+    )
+      res.status(400).send("Invalid Login Credentials");
+    else {
+      await page.goto(
+        "https://gu.icloudems.com/corecampus/student/attendance/subwise_attendace_new.php"
+      );
+      //
+      await page.waitForTimeout(1500);
+
+      //await page.click("#select2-acadyear-container")
+      //await (await page.$('.select2-selection__rendered')).press('Enter'); // Enter Key
+      //await (await page.$('#select2-acadyear-result-npqk-2021-2022')).press('Enter'); // Enter Key
+
+      await page.select("#acadyear", "2021-2022");
+      await page.waitForTimeout(1500);
+      await page.select("#classid", "2816");
+      await page.waitForTimeout(1500);
+      await (await page.$("#getattendance")).press("Enter"); // Enter Key
+
+      await page.waitForTimeout(2500);
+
+      const result = await page.$$eval(".table tr", (rows) => {
+        return Array.from(rows, (row) => {
+          const columns = row.querySelectorAll("td");
+          return Array.from(columns, (column) => column.textContent);
+        });
+      });
+
+      let final = [];
+
+      let final_percentage = {
+        total_classes: "",
+        percentage: "",
+      };
+
+      //get photo and name as well
+
+      //const img = await page.$$eval('.rounded-circle.rounded.img-rounded.image-cropper > img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+
+      const images = await page.$$eval("img", (anchors) =>
+        [].map.call(anchors, (img) => img.src)
+      );
+
+      const dp =
+        images[
+          images.findIndex((element) => element.includes("/student_profile"))
+        ];
+
+      const name = await page.evaluate(() => {
+        const elements = document.getElementsByClassName(
+          "d-none d-lg-inline-block mr-2"
+        );
+        return Array.from(elements).map((element) => element.innerText);
+      });
+
+      for (let i = 0; i < result.length; i++) {
+        let data = {
+          serial_number: "",
+          name: "",
+          subject_code: "",
+          code: "",
+          classes: "",
+          percentage: "",
+        };
+        for (let j = 0; j < result[i].length; j++) {
+          if (i === result.length - 1) {
+            if (j !== 0)
+              final_percentage[Object.keys(final_percentage)[j - 1]] =
+                result[i][j];
+          } else data[Object.keys(data)[j]] = result[i][j];
+        }
+        if (!isEmpty(final_percentage)) {
+          final.push(final_percentage);
+        }
+        if (!isEmpty(data)) {
+          final.push(data);
+        }
+      }
+
+      let metadata = { name, dp };
+      final.push({ metadata });
+
+      res.send(final);
+      await browser.close();
+    }
+    await browser.close();
+  }
+};
+
+export default handler;

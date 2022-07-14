7b92d3f1c86b6bf2e0077dc7fc3a96d19bf85fb4
areeburrub <areeburrub@gmail.com>
2022-07-14 20:28:06 +0530

    no-rules

--- pages/transaction/settelment/payout/unavailable.js
+++ pages/transaction/settelment/payout/unavailable.js
@@ -44,9 +44,9 @@ const Unavailable = () => {
         height="380"
         src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
         title="Rick Astley - Never Gonna Give You Up (Official Music Video)"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     );
   }

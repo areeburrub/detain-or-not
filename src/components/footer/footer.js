4e37a891f0b24d9b77750c11bb552a99ef048712
areeburrub <areeburrub@gmail.com>
2022-08-02 12:39:54 +0530

    fixed github link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <div>
       <nav className={styles.footer}>
         <div className={styles.logo}>
-          GET THE CODE <a href="https://github.com/areeburrub/" target='__blank'>@areeburrub</a>
+          GET THE CODE <a href="https://github.com/areeburrub/detain-or-not" target='__blank'>@areeburrub</a>
         </div>
       </nav>
     </div>
